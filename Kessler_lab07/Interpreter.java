894f9fdfa210fbf6e555c96d00af02bce4bff244
Caroline E. Kessler <19cek1@fuji.cs.williams.edu>
2016-10-26 19:40:27 -0400

    stuff

---
+++ Kessler_lab07/Interpreter.java
@@ -0,0 +1,50 @@
+import structure5.*;
+
+public class Interpreter {
+    StackVector<Token> myStack;
+    Token next;
+    
+    public Interpreter(){
+	myStack = new StackVector<Token>();
+    }
+
+    public static void main(String []args) {
+	Interpreter myInterpreter = new Interpreter();
+	Reader mainReader = new Reader();
+	myInterpreter.interpret(mainReader);
+    }
+
+    public void interpret(Reader r) {
+	while (r.hasNext()) {
+	    next = r.next();
+	    if (next.isNumber() || next.isBoolean()) {
+		myStack.push(next);
+	    } else if (next.isSymbol()) {
+		manageSymbol(next);
+	    } 
+	}
+    }
+
+    public void manageSymbol(Token s) {
+	String mySym = s.getSymbol();
+	if (mySym.equals("quit")) {
+	    System.exit(0);
+	} else if (mySym.equals("pstack")) {
+	    System.out.println(myStack.toString());
+	} else if (mySym.equals("add")) {
+	    if (!myStack.isEmpty()) {
+		Token t1 = myStack.pop();
+	    
+		if (myStack.isEmpty()) {
+		    Token t2 = myStack.pop();
+		    if (t1.isNumber() && t2.isNumber()) {
+			myStack.push(new Token
+				     (t1.getNumber() + t2.getNumber()));
+		    }
+		}
+	    }
+	}
+    }
+    
+}
+
